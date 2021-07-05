150fddd4b6e5426bacd4f6389772690f286da5fe
CraigSmith789 <craig789@gmail.com>
2021-07-05 19:21:32 -0400

    function add

--- main.rs
+++ main.rs
@@ -11,6 +11,17 @@ fn main() {
   let another_name = "Alice";
   println!("{} and {}", name, another_name);
 }
+fn main() {
+  let first = "Pascal".to_string();
+  let last = "Precht".to_string();
+  
+  say_name(first, last);
+}
+
+fn say_name(first: String, last: String) {
+  println!("{} {}", first, last);
+}
+
 
 
 
